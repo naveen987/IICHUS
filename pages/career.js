24053c67a47061b7f6bf07aeb99c82a6e2d01bd7
T S <nt090495@cerner.net>
2023-01-22 18:49:08 +0530

    career name changed

--- pages/career.js
+++ pages/career.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NavbarStyle2 from "@/components/Layout/Navbar/NavbarStyle2";
 import PageBanner from "@/components/Common/PageBanner";
-import Career from "@/components/career/career"
+import Career from "@/components/Career/career"
 import Footer from "@/components/Layout/Footer/Footer";
 
 export default function Contact() {

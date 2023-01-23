a904c616a0ed4f9f6afccf170f53d66b14f71514
Naveen <33518368+naveen987@users.noreply.github.com>
2023-01-23 11:54:21 +0530

    Update Domain.js

--- components/Domains/Domain.js
+++ components/Domains/Domain.js
@@ -20,7 +20,7 @@ const Domains = () => {
                     >
                       <div className="about-image">
                         <img
-                          src="/images/domain/retail.JPG"
+                          src="/images/domain/Retail.jpg"
                           alt="Teza"
                         />
                       </div>
@@ -93,7 +93,7 @@ const Domains = () => {
                     >
                       <div className="about-image">
                         <img
-                          src="/images/domain/insurance.JPG"
+                          src="/images/domain/Insurance.jpg"
                           alt="Teza"
                         />
                       </div>
@@ -108,7 +108,7 @@ const Domains = () => {
                       data-aos-once="true"
                     >
                       <div className="about-image">
-                        <img src="/images/domain/banking.JPG" alt="Teza" />
+                        <img src="/images/domain/Banking.jpg" alt="Teza" />
                       </div>
                     </div>
 
@@ -177,7 +177,7 @@ const Domains = () => {
                     >
                       <div className="about-image">
                         <img
-                          src={`/images/domain/logistic.JPG`}
+                          src={`/images/domain/Logistic.jpg`}
                           alt="Teza"
                         />
                       </div>

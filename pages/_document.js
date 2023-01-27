69158b9fdd344bd47ff4707a33f3fb88effa67cc
T S <nt090495@cerner.net>
2023-01-28 00:55:02 +0530

    path changed

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html lang="zxx">
         <Head>
-          <link rel="icon" href="/images/favicon.png" />
+          <link rel="icon" href="./images/favicon.png" />
           <link
             href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
             rel="stylesheet"

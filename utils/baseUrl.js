36dcb2b016b770e4edbcb409179947c2b5425915
T S <nt090495@cerner.net>
2023-01-28 00:00:56 +0530

    Base url ref change

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://teza-react.envytheme.com"
-    : "http://localhost:3000";
+    ? "https://teza-react.envytheme.com/IICHUS/index-2/"
+    : "http://localhost:3000/IICHUS/index-2/";
 
 export default baseUrl;
